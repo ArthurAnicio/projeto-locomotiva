84c538f5b472e075b1d2670aee2d32e7da2ddff4
ArthurAnicio <arthuranicio06@gmail.com>
2025-12-02 11:48:20 -0300

    Tentativa de configuração

--- __tests__/Home.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import Home from "../src/app/page"
-
-describe("Home Page", () => {
-  it("should render the home page", () => {
-    render(<Home />);
-  })
-})
---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+import type {Config} from 'jest';
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const config: Config = {
+  
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts']
+
+};
+
+module.exports = createJestConfig(config)
